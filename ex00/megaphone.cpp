0731ce6e43f06d61959d9480f748d55c0ae914a1
OMAR-DIB <omardib759@gmail.com>
2024-09-23 00:49:17 +1400

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odib <odib@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 23:35:11 by odib              #+#    #+#             */
+/*   Updated: 2024/09/22 00:06:24 by odib             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+int main(int ac, char **av){
+	if (ac > 1)
+	{
+		for (int i = 1; i<ac;i++){			
+			for (int j =0; av[i][j];j++){
+				std::cout<<(char)std::toupper((char)av[i][j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout<<"*LOUD AND UNBEARABLE FEEDBACK NOISE*"<<std::endl;	
+	}	
+	return 0;
+}
